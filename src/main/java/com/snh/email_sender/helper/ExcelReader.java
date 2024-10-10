9e5a87600447ccb07cf4b80bceb9fc66d409920c
mdaziz22 <mdazizmda@gmail.com>
2024-10-10 19:01:50 +0530

    Added new features

--- src/main/java/com/snh/email_sender/helper/ExcelReader.java
+++ src/main/java/com/snh/email_sender/helper/ExcelReader.java
@@ -14,19 +14,25 @@ import java.util.Map;
 
 public class ExcelReader {
 
-    public static List<Map<String, String>> readEmailsFromExcel(String filePath) throws IOException {
-        List<Map<String, String>> recipientsList = new LinkedList<>();
+    public static List<String> readEmailsFromExcel(String filePath) throws IOException {
+        List<String> recipientsList = new LinkedList<>();
         ClassPathResource resource = new ClassPathResource(filePath);
 
         try (InputStream excelFile = resource.getInputStream()) {
 
             Workbook workbook = new XSSFWorkbook(excelFile);
-            Sheet sheet = workbook.getSheetAt(0);  // Assuming recipientsList are in the first sheet
+            Sheet sheet = workbook.getSheetAt(0);
 
             for (Row row : sheet) {
-                String recipientName = row.getCell(0).getStringCellValue();
-                String recipientEmail = row.getCell(1).getStringCellValue();
-                recipientsList.add(Map.of(recipientName, recipientEmail));
+                if(row.getCell(0)!=null && "Name".equalsIgnoreCase(row.getCell(0).getStringCellValue())){
+                    continue;
+                }
+                String recipientName = row.getCell(0)==null? "" : row.getCell(0).getStringCellValue();
+                String recipientEmail = row.getCell(1)==null? "" : row.getCell(1).getStringCellValue();
+                String companyName = row.getCell(2)==null? "" : row.getCell(2).getStringCellValue();
+                String jobTitle = row.getCell(3)==null? "" : row.getCell(3).getStringCellValue();
+                if(recipientEmail !="" && recipientName !="")
+                    recipientsList.add(recipientName+"#"+recipientEmail+"#"+companyName+"#"+jobTitle);
             }
 
             workbook.close();
--- src/main/java/com/snh/email_sender/service/EmailService.java
+++ src/main/java/com/snh/email_sender/service/EmailService.java
@@ -3,27 +3,58 @@ package com.snh.email_sender.service;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class EmailService {
 
+    @Value("${linkedInUrl}")
+    private String linkedInUrl;
+
+    @Value("${githubUrl}")
+    private String githubUrl;
+
+    @Value("${filePath}")
+    private String filePath;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendEmail(String from, List<String> bccRecipients, String subject, String body) throws MessagingException {
-        MimeMessage mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+    public void sendEmail(List<String> recipients, String body) throws MessagingException {
 
-        helper.setFrom(from);
-        helper.setSubject(subject);
-        helper.setText(body, true);
-        helper.setBcc(bccRecipients.toArray(new String[0]));
+        body = body.replace("[Platform/Source]", "LinkedIn");
+        body = body.replace("[LinkedIn]", linkedInUrl);
+        body = body.replace("[GitHub]", githubUrl);
 
-        mailSender.send(mimeMessage);
+        for(int i = 0; i<recipients.size(); i++){
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+            String[] data = recipients.get(i).split("#");
+            String subject = "Application for "+ data[3];
+            body = body.replace("[Recruiter’s Name]", data[0]);
+            body = body.replace("[Job Title]", data[3]);
+            body = body.replace("[Company Name]", data[2]);
+
+            File file = new File(filePath);
+            helper.setFrom(fromEmail);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            helper.setTo(data[1]);
+            helper.addAttachment(file.getName(), file);
+
+            System.out.println("Sent Mail to " + data[1]);
+            mailSender.send(mimeMessage);
+        }
     }
 }
